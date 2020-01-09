c455d455683266f789c3fc9b378f0f6521f9db1b
Donato Meoli <donato.meoli.95@gmail.com>
2020-01-09 17:25:36 +0100

    added tests

--- tests/test_accelerated_gradient.py
+++ tests/test_accelerated_gradient.py
@@ -1,4 +1,43 @@
+import numpy as np
 import pytest
+
+from optimization import gen_quad_1, gen_quad_2, gen_quad_5, Rosenbrock
+from optimization.unconstrained.accelerated_gradient import AcceleratedGradient
+
+
+def test_quadratic():
+    x, status = AcceleratedGradient(gen_quad_1).minimize()
+    assert np.allclose(x, gen_quad_1.x_star)
+    assert status is 'optimal'
+
+    x, status = AcceleratedGradient(gen_quad_2).minimize()
+    assert np.allclose(x, gen_quad_2.x_star)
+    assert status is 'optimal'
+
+    # x, status = AcceleratedGradient(gen_quad_3).minimize()
+    # assert np.allclose(x, gen_quad_3.x_star)
+    # assert status is 'optimal'
+
+    # x, status = AcceleratedGradient(gen_quad_4).minimize()
+    # assert np.allclose(x, gen_quad_4.x_star)
+    # assert status is 'optimal'
+
+    x, status = AcceleratedGradient(gen_quad_5).minimize()
+    assert np.allclose(x, gen_quad_5.x_star)
+    assert status is 'optimal'
+
+
+def test_Rosenbrock():
+    obj = Rosenbrock(autodiff=True)
+    x, status = AcceleratedGradient(obj).minimize()
+    assert np.allclose(x, obj.x_star, rtol=0.1)
+    assert status is 'optimal'
+
+    obj = Rosenbrock(autodiff=False)
+    x, status = AcceleratedGradient(obj).minimize()
+    assert np.allclose(x, obj.x_star, rtol=0.1)
+    assert status is 'optimal'
+
 
 if __name__ == "__main__":
     pytest.main()
