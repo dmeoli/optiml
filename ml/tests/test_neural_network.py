daa71e2a35897ac2c649a47c8e297b996ffaa7dd
Donato Meoli <donato.meoli.95@gmail.com>
2020-01-27 18:43:09 +0100

    fixed learning tests

--- ml/tests/test_neural_network.py
+++ ml/tests/test_neural_network.py
@@ -2,8 +2,18 @@ import numpy as np
 import pytest
 
 from ml.dataset import DataSet
-from ml.learning import err_ratio, grade_learner
 from ml.neural_network.neural_network import NeuralNetLearner, PerceptronLearner
+from ml.validation import err_ratio, grade_learner
+
+iris_tests = [([[5.0, 3.1, 0.9, 0.1]], 0),
+              ([[5.1, 3.5, 1.0, 0.0]], 0),
+              ([[4.9, 3.3, 1.1, 0.1]], 0),
+              ([[6.0, 3.0, 4.0, 1.1]], 1),
+              ([[6.1, 2.2, 3.5, 1.0]], 1),
+              ([[5.9, 2.5, 3.3, 1.1]], 1),
+              ([[7.5, 4.1, 6.2, 2.3]], 2),
+              ([[7.3, 4.0, 6.1, 2.4]], 2),
+              ([[7.0, 3.3, 6.1, 2.5]], 2)]
 
 
 def test_neural_net():
@@ -13,17 +23,8 @@ def test_neural_net():
     n_samples, n_features = len(iris.examples), iris.target
     X, y = np.array([x[:n_features] for x in iris.examples]), \
            np.array([x[n_features] for x in iris.examples])
-    tests = [([[5.0, 3.1, 0.9, 0.1]], 0),
-             ([[5.1, 3.5, 1.0, 0.0]], 0),
-             ([[4.9, 3.3, 1.1, 0.1]], 0),
-             ([[6.0, 3.0, 4.0, 1.1]], 1),
-             ([[6.1, 2.2, 3.5, 1.0]], 1),
-             ([[5.9, 2.5, 3.3, 1.1]], 1),
-             ([[7.5, 4.1, 6.2, 2.3]], 2),
-             ([[7.3, 4.0, 6.1, 2.4]], 2),
-             ([[7.0, 3.3, 6.1, 2.5]], 2)]
     nnl = NeuralNetLearner([4]).fit(X, y)
-    assert grade_learner(nnl, tests) >= 1 / 3
+    assert grade_learner(nnl, iris_tests) >= 1 / 3
     assert err_ratio(nnl, X, y) < 0.21
 
 
@@ -35,16 +36,7 @@ def test_perceptron():
     X, y = np.array([x[:n_features] for x in iris.examples]), \
            np.array([x[n_features] for x in iris.examples])
     pl = PerceptronLearner().fit(X, y)
-    tests = [([[5.0, 3.1, 0.9, 0.1]], 0),
-             ([[5.1, 3.5, 1.0, 0.0]], 0),
-             ([[4.9, 3.3, 1.1, 0.1]], 0),
-             ([[6.0, 3.0, 4.0, 1.1]], 1),
-             ([[6.1, 2.2, 3.5, 1.0]], 1),
-             ([[5.9, 2.5, 3.3, 1.1]], 1),
-             ([[7.5, 4.1, 6.2, 2.3]], 2),
-             ([[7.3, 4.0, 6.1, 2.4]], 2),
-             ([[7.0, 3.3, 6.1, 2.5]], 2)]
-    assert grade_learner(pl, tests) > 1 / 2
+    assert grade_learner(pl, iris_tests) > 1 / 2
     assert err_ratio(pl, X, y) < 0.4
 
 
