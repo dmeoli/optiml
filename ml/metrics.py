68df80c81fb12026b3ff0ea89bb21fa5bebd3cc9
Donato Meoli <donato.meoli.95@gmail.com>
2020-02-21 20:24:17 +0100

    updated metrics

--- ml/metrics.py
+++ ml/metrics.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def accuracy_score(y_true, y_pred):
     assert y_true.shape == y_pred.shape
-    return np.where(y_true == y_pred, 1., 0.).mean()
+    return np.mean(np.equal(y_true, y_pred))
 
 
 def mean_squared_error(y_true, y_pred):
@@ -18,4 +18,4 @@ def mean_absolute_error(y_true, y_pred):
 
 def mean_euclidean_error(y_true, y_pred):
     assert y_true.shape == y_pred.shape
-    return np.sum([np.linalg.norm(t - p) for t, p in zip(y_true, y_pred)]) / y_true.shape[0]
+    return np.mean([np.linalg.norm(t - p) for t, p in zip(y_true, y_pred)])
