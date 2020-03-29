caa2dc1f06774e6001f72b6ad01964bd4c6a2ddb
Donato Meoli <donato.meoli.95@gmail.com>
2020-03-29 18:22:10 +0200

    fixed PBM tests

--- optimization/tests/test_proximal_bundle.py
+++ optimization/tests/test_proximal_bundle.py
@@ -3,15 +3,13 @@ import pytest
 
 from optimization.optimization_function import quad1, quad2, Rosenbrock
 from optimization.unconstrained.proximal_bundle import ProximalBundle
-from utils import not_test
 
 
 def test_quadratic():
     assert np.allclose(ProximalBundle(quad1).minimize()[0], quad1.x_star())
-    assert np.allclose(ProximalBundle(quad2).minimize()[0], quad2.x_star(), rtol=1e-4)
+    assert np.allclose(ProximalBundle(quad2).minimize()[0], quad2.x_star())
 
 
-@not_test
 def test_Rosenbrock():
     obj = Rosenbrock()
     assert np.allclose(ProximalBundle(obj).minimize()[0], obj.x_star())
