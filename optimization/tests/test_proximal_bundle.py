20caf54ccfd847e65d25793b6276554835e7ee6d
Donato Meoli <donato.meoli.95@gmail.com>
2020-03-29 18:25:53 +0200

    added more tests to PBM

--- optimization/tests/test_proximal_bundle.py
+++ optimization/tests/test_proximal_bundle.py
@@ -15,5 +15,25 @@ def test_Rosenbrock():
     assert np.allclose(ProximalBundle(obj).minimize()[0], obj.x_star())
 
 
+def test_standard_momentum_quadratic():
+    assert np.allclose(ProximalBundle(quad1, momentum_type='standard').minimize()[0], quad1.x_star())
+    assert np.allclose(ProximalBundle(quad2).minimize()[0], quad2.x_star())
+
+
+def test_standard_momentum_Rosenbrock():
+    obj = Rosenbrock()
+    assert np.allclose(ProximalBundle(obj, momentum_type='standard').minimize()[0], obj.x_star())
+
+
+def test_Nesterov_momentum_quadratic():
+    assert np.allclose(ProximalBundle(quad1, momentum_type='nesterov').minimize()[0], quad1.x_star())
+    assert np.allclose(ProximalBundle(quad2, momentum_type='nesterov').minimize()[0], quad2.x_star())
+
+
+def test_Nesterov_momentum_Rosenbrock():
+    obj = Rosenbrock()
+    assert np.allclose(ProximalBundle(obj, momentum_type='nesterov').minimize()[0], obj.x_star())
+
+
 if __name__ == "__main__":
     pytest.main()
