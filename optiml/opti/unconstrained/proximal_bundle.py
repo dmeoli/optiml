3dbb642a156a376724c13875159ed4ce232cbddd
Donato Meoli <donato.meoli.95@gmail.com>
2020-06-01 13:57:30 +0200

    updated

--- optiml/opti/unconstrained/proximal_bundle.py
+++ optiml/opti/unconstrained/proximal_bundle.py
@@ -73,9 +73,26 @@ class ProximalBundle(Optimizer):
     #     number of iterations: x is the bast solution found so far, but not
     #     necessarily the optimal one
 
-    def __init__(self, f, x, mu=1, m1=0.01, eps=1e-6, max_iter=1000, m_inf=-np.inf, master_solver='ecos',
-                 callback=None, callback_args=(), verbose=False, master_verbose=False):
-        super().__init__(f, x, eps, max_iter, callback, callback_args, verbose)
+    def __init__(self,
+                 f,
+                 x,
+                 mu=1,
+                 m1=0.01,
+                 eps=1e-6,
+                 max_iter=1000,
+                 m_inf=-np.inf,
+                 master_solver='ecos',
+                 callback=None,
+                 callback_args=(),
+                 verbose=False,
+                 master_verbose=False):
+        super().__init__(f=f,
+                         x=x,
+                         eps=eps,
+                         max_iter=max_iter,
+                         callback=callback,
+                         callback_args=callback_args,
+                         verbose=verbose)
         if not mu > 0:
             raise ValueError('mu must be > 0')
         self.mu = mu
