fixed non-posdef hessian case

optiml/ml/svm/_base.py
@@ -901,10 +901,10 @@ class DualSVR(RegressorMixin, DualSVM):
 
                 elif issubclass(self.optimizer, Optimizer):
 
-                    Q = Q + np.outer(A, A)
-                    self.obj = Quadratic(Q, q)
+                    # Q = Q + np.outer(A, A)
+                    # self.obj = Quadratic(Q, q)
 
-                    self.obj = LagrangianBoxConstrainedQuadratic(self.obj, ub)
+                    self.obj = LagrangianConstrainedQuadratic(self.obj, A, ub)
                     self.optimizer = LagrangianDual(f=self.obj,
                                                     optimizer=self.optimizer,
                                                     step_size=self.learning_rate_init,

optiml/opti/_base.py
@@ -1,8 +1,5 @@
 import autograd.numpy as np
 from autograd import jacobian, hessian
-from scipy.linalg import ldl
-
-from optiml.opti.utils import ldl_solve
 
 
 class Optimizer:
@@ -127,8 +124,8 @@ class Quadratic(OptimizationFunction):
     def x_star(self):
         if not hasattr(self, 'x_opt'):
             try:
-                self.x_opt = ldl_solve(ldl(self.Q), -self.q)
-            except np.linalg.LinAlgError:  # the Hessian matrix is singular
+                self.x_opt = np.linalg.solve(self.Q, -self.q)
+            except np.linalg.LinAlgError:
                 self.x_opt = np.full(fill_value=np.nan, shape=self.ndim)
         return self.x_opt
 

optiml/opti/constrained/bcqp/_base.py
@@ -1,11 +1,10 @@
 from abc import ABC
 
 import numpy as np
-from scipy.linalg import ldl
 
 from ... import Optimizer
 from ... import Quadratic
-from ...utils import ldl_solve
+from ...utils import cholesky_solve, nearest_posdef
 
 
 class BoxConstrainedQuadraticOptimizer(Optimizer, ABC):
@@ -42,12 +41,9 @@ class LagrangianBoxConstrainedQuadratic(Quadratic):
     def __init__(self, quad, ub):
         if not isinstance(quad, Quadratic):
             raise TypeError(f'{quad} is not an allowed quadratic function')
-        super().__init__(quad.Q, quad.q)
+        super().__init__(nearest_posdef(quad.Q), quad.q)
         self.ndim *= 2
-        # Compute the LDL^T Cholesky symmetric indefinite factorization
-        # of Q because it is symmetric but could be not positive definite.
-        # This will be used at each iteration to solve the Lagrangian relaxation.
-        self.L, self.D, self.P = ldl(self.Q)
+        self.L = np.linalg.cholesky(self.Q)
         if any(u < 0 for u in ub):
             raise ValueError('the lower bound must be > 0')
         self.ub = np.asarray(ub, dtype=np.float)
@@ -92,7 +88,7 @@ class LagrangianBoxConstrainedQuadratic(Quadratic):
         if np.array_equal(lmbda, self.last_lmbda):
             x = self.last_x
         else:
-            x = ldl_solve((self.L, self.D, self.P), -ql)
+            x = cholesky_solve(self.L, -ql)
             self.last_lmbda = lmbda
             self.last_x = x
         return (0.5 * x.T.dot(self.Q) + ql.T).dot(x) - lmbda_p.T.dot(self.ub)
@@ -116,7 +112,7 @@ class LagrangianBoxConstrainedQuadratic(Quadratic):
         else:
             lmbda_p, lmbda_n = np.split(lmbda, 2)
             ql = self.q + lmbda_p - lmbda_n
-            x = ldl_solve((self.L, self.D, self.P), -ql)
+            x = cholesky_solve(self.L, -ql)
             self.last_lmbda = lmbda
             self.last_x = x
         g = np.hstack((self.ub - x, x))

optiml/opti/constrained/bcqp/active_set.py
@@ -1,8 +1,7 @@
 import numpy as np
-from scipy.linalg import ldl
 
 from . import BoxConstrainedQuadraticOptimizer
-from ...utils import ldl_solve
+from ...utils import cholesky_solve, nearest_posdef
 
 
 class ActiveSet(BoxConstrainedQuadraticOptimizer):
@@ -101,7 +100,8 @@ class ActiveSet(BoxConstrainedQuadraticOptimizer):
 
             # use the LDL^T Cholesky symmetric indefinite factorization to solve the
             # linear system since Q_{AA} is symmetric but could be not positive definite
-            xs[A] = ldl_solve(ldl(self.f.Q[A, :][:, A]), -(self.f.q[A] + self.f.Q[A, :][:, U].dot(self.ub[U])))
+            xs[A] = cholesky_solve(np.linalg.cholesky(nearest_posdef(self.f.Q[A, :][:, A])),
+                                   -(self.f.q[A] + self.f.Q[A, :][:, U].dot(self.ub[U])))
 
             if np.logical_and(xs[A] <= self.ub[A] + 1e-12, xs[A] >= -1e-12).all():
                 # the solution of the unconstrained problem is actually feasible

optiml/opti/constrained/lagrangian_dual.py
@@ -1,11 +1,10 @@
 import numpy as np
-from scipy.linalg import ldl
 
 from .. import Optimizer, Quadratic
 from ..unconstrained.line_search import LineSearchOptimizer
 from ..unconstrained.stochastic import StochasticOptimizer, AdaGrad
 from ..unconstrained.stochastic.schedules import constant
-from ..utils import ldl_solve
+from ..utils import cholesky_solve, nearest_posdef
 
 
 class LagrangianDual(Optimizer):
@@ -118,12 +117,9 @@ class LagrangianConstrainedQuadratic(Quadratic):
     def __init__(self, quad, A, ub):
         if not isinstance(quad, Quadratic):
             raise TypeError(f'{quad} is not an allowed quadratic function')
-        super().__init__(quad.Q, quad.q)
+        super().__init__(nearest_posdef(quad.Q), quad.q)
         self.ndim *= 3
-        # Compute the LDL^T Cholesky symmetric indefinite factorization
-        # of Q because it is symmetric but could be not positive definite.
-        # This will be used at each iteration to solve the Lagrangian relaxation.
-        self.L, self.D, self.P = ldl(self.Q)
+        self.L = np.linalg.cholesky(self.Q)
         self.A = np.asarray(A, dtype=np.float)
         if any(u < 0 for u in ub):
             raise ValueError('the lower bound must be > 0')
@@ -162,7 +158,7 @@ class LagrangianConstrainedQuadratic(Quadratic):
         """
         mu, lmbda_p, lmbda_n = np.split(lmbda, 3)
         ql = self.q - mu.dot(self.A) + lmbda_p - lmbda_n
-        x = ldl_solve((self.L, self.D, self.P), -ql)
+        x = cholesky_solve(self.L, -ql)
         return 0.5 * x.T.dot(self.Q).dot(x) + ql.T.dot(x) - lmbda_p.T.dot(self.ub)
 
     def jacobian(self, lmbda):
@@ -182,7 +178,7 @@ class LagrangianConstrainedQuadratic(Quadratic):
         """
         mu, lmbda_p, lmbda_n = np.split(lmbda, 3)
         ql = self.q - mu.dot(self.A) + lmbda_p - lmbda_n
-        x = ldl_solve((self.L, self.D, self.P), -ql)
+        x = cholesky_solve(self.L, -ql)
         g = np.hstack((self.A * x, self.ub - x, x))
 
         # compute an heuristic solution out of the solution x of