2e98d6e9205e9ee4754354eb1d2b8d8989fa798c
Donato Meoli <donato.meoli.95@gmail.com>
2021-05-28 01:51:48 +0200

    Update utils.py

--- optiml/opti/utils.py
+++ optiml/opti/utils.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from casadi import ldl_solve, ldl
 from matplotlib.colors import SymLogNorm
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from scipy.sparse.linalg import minres
@@ -7,7 +8,7 @@ from scipy.sparse.linalg import minres
 from .unconstrained import ProximalBundle
 
 
-def solve_lagrangian_equality_constrained_quadratic(Q, q, A):
+def solve_lagrangian_equality_constrained_quadratic(Q, q, A, method='minres'):
     """
     Solve a quadratic function subject to equality constraint:
 
@@ -19,12 +20,35 @@ def solve_lagrangian_equality_constrained_quadratic(Q, q, A):
             | A  0  | | lmbda |   |  b = 0 |
     """
     A = np.atleast_2d(A).astype(float)
-    kkt_Q = np.vstack((np.hstack((Q, A.T)),
-                       np.hstack((A, np.zeros((A.shape[0], A.shape[0]))))))
-    kkt_q = np.hstack((-q, np.zeros((A.shape[0],))))
-    x_lmbda = minres(kkt_Q, kkt_q)[0]
-    # assert np.allclose(x_lmbda[:-A.shape[0]], solve_qp(P=Q, q=q, A=A, b=np.zeros(1), solver='cvxopt'))
-    return x_lmbda[:-A.shape[0]]
+    if method in ('minres', 'ldl'):
+        kkt_Q = np.vstack((np.hstack((Q, A.T)),
+                           np.hstack((A, np.zeros((A.shape[0], A.shape[0]))))))
+        kkt_q = np.hstack((-q, np.zeros((A.shape[0],))))
+        if method == 'minres':
+            x_lmbda = minres(kkt_Q, kkt_q)[0]
+        elif method == 'ldl':
+            #  https://www.math.uh.edu/~rohop/fall_06/Chapter3.pdf#page=3
+            x_lmbda = np.array(ldl_solve(kkt_q, *ldl(kkt_Q))).ravel()
+        # assert np.allclose(x_lmbda[:-A.shape[0]], solve_qp(P=Q, q=q, A=A, b=np.zeros(1), solver='cvxopt'))
+        return x_lmbda[:-A.shape[0]], x_lmbda[-A.shape[0]:]
+    elif method == 'nullspace':
+        q, r = np.linalg.qr(A.T, mode='complete')
+        Z = q[:, A.shape[0]:]  # orthonormal basis for the null space of A, i.e., kernel(A)
+        assert np.allclose(A.dot(Z), 0)
+        Y = q[:, :A.shape[0]]
+        Rp = r[:A.shape[0], :]
+        x_special = np.linalg.lstsq(A, -np.array([0]), rcond=None)[0]
+        reduced_Q = Z.T.dot(Q).dot(Z)
+        reduced_q = Z.T.dot(q) + Z.T.dot(Q).dot(x_special)
+        x_g = np.linalg.lstsq(reduced_Q, -reduced_q, rcond=None)[0]
+        x = Z.dot(x_g) + x_special
+        lmbda = np.linalg.lstsq(Rp, -Y.T.dot(Q.dot(x) + q), rcond=None)[0]
+        return x, lmbda
+    elif method == 'schur':
+        # http://www.cs.nthu.edu.tw/~cherung/teaching/2009cs5321/link/chap16ex.pdf#page=2
+        raise NotImplementedError
+    else:
+        raise NotImplementedError
 
 
 # bcqp generator
@@ -139,7 +163,12 @@ def generate_box_constrained_quadratic(ndim=2, actv=0.5, rank=1.1, ecc=0.99, ub_
 
 # plot functions
 
-def plot_surface_contour(f, x_min, x_max, y_min, y_max, A=None, ub=None):
+def plot_surface_contour(f, x_min, x_max, y_min, y_max):
+    dual = None
+    if hasattr(f, 'primal'):  # lagrangian dual
+        dual = f
+        f = dual.primal
+
     X, Y = np.meshgrid(np.arange(x_min, x_max, 0.1), np.arange(y_min, y_max, 0.1))
 
     Z = np.array([f(np.array([x, y]))
@@ -149,19 +178,28 @@ def plot_surface_contour(f, x_min, x_max, y_min, y_max, A=None, ub=None):
 
     # 3D surface plot
     ax = surface_contour.add_subplot(1, 2, 1, projection='3d', elev=50, azim=-50)
-    surf = ax.plot_surface(X, Y, Z, norm=SymLogNorm(linthresh=abs(Z.min()), base=np.e), cmap='jet', alpha=0.5)
-    # bug https://stackoverflow.com/a/55534939/5555994
-    surf._facecolors2d = surf._facecolor3d
-    surf._edgecolors2d = surf._edgecolor3d
+    ax.plot_surface(X, Y, Z, norm=SymLogNorm(linthresh=abs(Z.min()), base=np.e), cmap='jet', alpha=0.5)
     ax.plot(*f.x_star(), f.f_star(), marker='*', color='r', markersize=10)
     ax.set_xlabel('$x_1$')
     ax.set_ylabel('$x_2$')
     ax.set_zlabel(f'${type(f).__name__}$')
 
-    if ub is not None:
+    if dual and hasattr(dual, 'A'):
+        X, Y = np.meshgrid(np.arange(x_min, x_max, 2), np.arange(y_min, y_max, 2))
+        Z = np.array([f(np.array([x, y]))
+                      for x, y in zip(X.ravel(), Y.ravel())]).reshape(X.shape)
+        # y = m x + q => m = -(A[0] / A[1]), q = 0
+        surf1 = ax.plot_surface(X, -(dual.A[0] / dual.A[1]) * X, Z, color='b', label='$Ax=0$')
+        # bug https://stackoverflow.com/a/55534939/5555994
+        surf1._facecolors2d = surf1._facecolor3d
+        surf1._edgecolors2d = surf1._edgecolor3d
+
+    if dual and hasattr(dual, 'ub'):  # and so also `lb`
+
         # 3D box-constraints plot
         z_min, z_max = Z.min(), Z.max()
         # vertices of the box
+        ub = dual.ub
         v = np.array([[ub[0], 0, z_min], [0, 0, z_min],
                       [0, ub[1], z_min], [ub[0], ub[1], z_min],
                       [ub[0], 0, z_max], [0, 0, z_max],
@@ -174,16 +212,36 @@ def plot_surface_contour(f, x_min, x_max, y_min, y_max, A=None, ub=None):
                  [v[1], v[2], v[6], v[5]],
                  [v[4], v[7], v[3], v[0]]]
         # plot sides
-        ax.add_collection3d(Poly3DCollection(verts, facecolors='k', alpha=0.1,
-                                             edgecolors='r', linewidths=1.,
-                                             label='$0 \leq x \leq ub$'))
+        surf2 = ax.add_collection3d(Poly3DCollection(verts, facecolors='k', edgecolors='k',
+                                                     alpha=0.1, label='$0 \leq x \leq ub$'))
+        # bug https://stackoverflow.com/a/55534939/5555994
+        surf2._facecolors2d = surf2._facecolor3d
+        surf2._edgecolors2d = surf2._edgecolor3d
 
-    if A is not None:
-        X, Y = np.meshgrid(np.arange(x_min, x_max, 2), np.arange(y_min, y_max, 2))
-        Z = np.array([f(np.array([x, y]))
-                      for x, y in zip(X.ravel(), Y.ravel())]).reshape(X.shape)
-        # y = m x + q => m = -(A[0] / A[1]), q = 0
-        ax.plot_surface(X, -(A[0] / A[1]) * X, Z, color='b', label='$Ax=0$')
+    elif dual and hasattr(dual, 'lb'):
+
+        # 3D box-constraints plot
+        z_min, z_max = Z.min(), Z.max()
+        # vertices of the box
+        v = np.array([[x_max, 0, z_min], [0, 0, z_min],
+                      [0, y_max, z_min], [x_max, y_max, z_min],
+                      [x_max, 0, z_max], [0, 0, z_max],
+                      [0, y_max, z_max], [x_max, y_max, z_max]])
+        # generate list of sides' polygons of our box
+        verts = [[v[0], v[1], v[2], v[3]],
+                 [v[4], v[5], v[6], v[7]],
+                 [v[0], v[1], v[5], v[4]],
+                 [v[2], v[3], v[7], v[6]],
+                 [v[1], v[2], v[6], v[5]],
+                 [v[4], v[7], v[3], v[0]]]
+        # plot sides
+        surf3 = ax.add_collection3d(Poly3DCollection(verts, facecolors='k', edgecolors='k',
+                                                     alpha=0.1, label='$x \geq 0$'))
+        # bug https://stackoverflow.com/a/55534939/5555994
+        surf3._facecolors2d = surf3._facecolor3d
+        surf3._edgecolors2d = surf3._edgecolor3d
+
+    ax.legend()
 
     # 2D contour plot
     ax = surface_contour.add_subplot(1, 2, 2)
@@ -192,24 +250,35 @@ def plot_surface_contour(f, x_min, x_max, y_min, y_max, A=None, ub=None):
     ax.set_xlabel('$x_1$')
     ax.set_ylabel('$x_2$')
 
-    if ub is not None:
+    if dual and hasattr(dual, 'A'):
+        X = np.arange(x_min, x_max, 2)
+        # y = m x + q => m = -(A[0] / A[1]), q = 0
+        ax.plot(X, -(dual.A[0] / dual.A[1]) * X, color='b')
+
+    if dual and hasattr(dual, 'ub'):  # and so also `lb`
+
         # 2D box-constraints plot
+        ub = dual.ub
         ax.plot([0, 0, ub[0], ub[0], 0],
-                [0, ub[1], ub[1], 0, 0], color='r', linewidth=1.5)
+                [0, ub[1], ub[1], 0, 0], color='k')
         ax.fill_between([0, ub[0]],
                         [0, 0],
                         [ub[1], ub[1]], color='0.8')
 
-    if A is not None:
-        X = np.arange(x_min, x_max, 2)
-        # y = m x + q => m = -(A[0] / A[1]), q = 0
-        ax.plot(X, -(A[0] / A[1]) * X, color='b', linewidth=1.5)
+    elif dual and hasattr(dual, 'lb'):
+
+        x_max = ax.get_xlim()[1]
+        y_max = ax.get_ylim()[1]
+        ax.plot([0, 0, x_max, x_max, 0],
+                [0, y_max, y_max, 0, 0], color='k')
+        ax.fill_between([0, x_max],
+                        [0, 0],
+                        [y_max, y_max], color='0.8')
 
     return surface_contour
 
 
-def plot_trajectory_optimization(surface_contour, opt, color='k', label=None,
-                                 linestyle='None', linewidth=1.):
+def plot_trajectory_optimization(surface_contour, opt, color='k', label=None, linewidth=1.5):
     # 3D trajectory optimization plot
     surface_contour.axes[0].plot(opt.x0_history, opt.x1_history, opt.f_x_history,
                                  marker='.', color=color, label=label, linewidth=linewidth)
@@ -217,8 +286,7 @@ def plot_trajectory_optimization(surface_contour, opt, color='k', label=None,
     angles_y = np.array(opt.x1_history)[1:] - np.array(opt.x1_history)[:-1]
     # 2D trajectory optimization plot
     surface_contour.axes[1].quiver(opt.x0_history[:-1], opt.x1_history[:-1], angles_x, angles_y,
-                                   scale_units='xy', angles='xy', scale=1, color=color,
-                                   linestyle=linestyle, linewidth=linewidth)
+                                   scale_units='xy', angles='xy', scale=1, color=color, linewidth=linewidth)
     if isinstance(opt, ProximalBundle):  # plot ns steps
         # 3D trajectory optimization plot
         surface_contour.axes[0].plot(opt.x0_history_ns, opt.x1_history_ns, opt.f_x_history_ns,
@@ -228,11 +296,11 @@ def plot_trajectory_optimization(surface_contour, opt, color='k', label=None,
         # 2D trajectory optimization plot
         surface_contour.axes[1].quiver(opt.x0_history_ns[:-1], opt.x1_history_ns[:-1], angles_x, angles_y,
                                        scale_units='xy', angles='xy', scale=1, color='b')
-    # surface_contour.axes[0].legend()
+    surface_contour.axes[0].legend()
     return surface_contour
 
 
-def plot_surface_trajectory_optimization(f, opt, x_min, x_max, y_min, y_max, A=None, ub=None,
-                                         color='k', label=None, linestyle='None', linewidth=1.):
-    return plot_trajectory_optimization(plot_surface_contour(f, x_min, x_max, y_min, y_max, A, ub),
-                                        opt, color, label, linestyle, linewidth)
+def plot_surface_trajectory_optimization(f, opt, x_min, x_max, y_min, y_max,
+                                         color='k', label=None, linewidth=1.5):
+    return plot_trajectory_optimization(plot_surface_contour(f, x_min, x_max, y_min, y_max),
+                                        opt, color, label, linewidth)
