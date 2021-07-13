331011991d8252348b091e0bc996deb5cfe92a46
Donato Meoli <donato.meoli.95@gmail.com>
2021-07-13 12:03:51 +0200

    updated config files

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
-from setuptools import setup
+import setuptools
 
-with open("README.md", "r") as file_stream:
-    readme = file_stream.read()
-
-setup(name="optiml",
-      version="0.5.5",
-      author="Donato Meoli",
-      author_email="donato.meoli.95@gmail.com",
-      description="Optimizers for/and sklearn compatible Machine Learning models",
-      long_description=readme,
-      long_description_content_type="text/markdown",
-      license="MIT",
-      url="https://github.com/dmeoli/optiml",
-      python_requires=">=3.6")
+setuptools.setup(name='optiml',
+                 version='1.0',
+                 author='Donato Meoli',
+                 author_email='donato.meoli.95@gmail.com',
+                 description='Optimizers for/and sklearn compatible Machine Learning models',
+                 long_description=open('README.md').read(),
+                 long_description_content_type='text/markdown',
+                 license='MIT',
+                 url='https://github.com/dmeoli/optiml',
+                 packages=setuptools.find_packages(),
+                 install_requires=open('requirements.txt').read().splitlines(),
+                 python_requires='>=3.6')
